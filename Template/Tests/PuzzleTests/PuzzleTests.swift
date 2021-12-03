5c1280318a7ef48ed2da2236f351e4dd5c6ce8e7
Frank Guchelaar <fguchelaar@gmail.com>
2021-12-03 23:07:14 +0100

    updated test-template

--- Template/Tests/PuzzleTests/PuzzleTests.swift
+++ Template/Tests/PuzzleTests/PuzzleTests.swift
@@ -2,8 +2,17 @@
 import XCTest
 
 final class PuzzleTests: XCTestCase {
-    func testExample() throws {
-        let puzzle = Puzzle(input: "")
+
+    let input = """
+    """
+
+    func testPart1() throws {
+        let puzzle = Puzzle(input: input)
         XCTAssertEqual(puzzle.part1(), "")
+    }
+
+    func testPart2() throws {
+        let puzzle = Puzzle(input: input)
+        XCTAssertEqual(puzzle.part2(), "")
     }
 }
