e8667c833638ae56e56a0cd5da5cc8bf22f5152c
Frank Guchelaar <fguchelaar@gmail.com>
2021-12-03 23:08:14 +0100

    remove built files and time the runs

--- buildAll.sh
+++ buildAll.sh
@@ -2,4 +2,4 @@
 
 find -s . -name "*day*" -depth 1 -type d \
     -exec echo Building {} \; \
-    -exec sh -c 'pushd . > /dev/null; cd $1; swift build -c release; popd > /dev/null' sh {} \;
+    -exec sh -c 'pushd . > /dev/null; cd $1; rm -rf .build; swift build -c release; popd > /dev/null' sh {} \;
--- runAll.sh
+++ runAll.sh
@@ -2,4 +2,4 @@
 
 find -s . -name "*day*" -depth 1 -type d \
     -exec echo Running {} \; \
-    -exec sh -c 'pushd .  > /dev/null; cd $1; swift run -c release; popd > /dev/null' sh {} \;
+    -exec sh -c 'pushd .  > /dev/null; cd $1; time swift run -c release; popd > /dev/null' sh {} \;
