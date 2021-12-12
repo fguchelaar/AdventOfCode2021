a8e5f53353cd5a0456aa9457cfc23269c827cb62
Frank Guchelaar <fguchelaar@gmail.com>
2021-12-12 23:47:30 +0100

    still ugly and rather slow, but got it down to ~2,5 seconds

--- day-12-passage-pathing/Sources/Puzzle/Puzzle.swift
+++ day-12-passage-pathing/Sources/Puzzle/Puzzle.swift
@@ -1,22 +1,73 @@
 import AdventKit
 import Foundation
 
+struct Cave {
+    var name: String
+    var isBig: Bool
+
+    init(_ name: String) {
+        self.name = name
+        self.isBig = name == name.uppercased()
+    }
+}
+
+extension Cave: Hashable {}
+
+class CaveNode {
+    var cave: Cave
+    var parent: CaveNode?
+    var children: [CaveNode]
+
+    var isEnd: Bool
+
+    init(_ cave: Cave) {
+        self.cave = cave
+        self.isEnd = cave.name == "end"
+        self.children = []
+    }
+
+    func addChild(caveNode: CaveNode) {
+        caveNode.parent = self
+        children.append(caveNode)
+    }
+
+    func pathContains(cave: Cave) -> Bool {
+        self.cave == cave || (parent?.pathContains(cave: cave) ?? false)
+    }
+
+    func pathContainsDoubleVisitedSmallCaves() -> Bool {
+        return pathContainsDoubleVisitedSmallCaves(set: Set<Cave>())
+    }
+
+    private func pathContainsDoubleVisitedSmallCaves(set: Set<Cave>) -> Bool {
+        var _set = set
+        if !_set.insert(cave).inserted, !cave.isBig {
+            return true
+        } else if let parent = parent {
+            return parent.pathContainsDoubleVisitedSmallCaves(set: _set)
+        } else {
+            return false
+        }
+    }
+}
+
 public class Puzzle {
-    let map: [String: [String]]
+    let map: [Cave: [Cave]]
+
     public init(input: String) {
         self.map = input
             .trimmingCharacters(in: .whitespacesAndNewlines)
             .components(separatedBy: .newlines)
-            .reduce(into: [String: [String]]()) { dict, line in
-                let caves = line.components(separatedBy: "-")
-                if caves[0] == "start" {
-                    dict["start", default: []].append(caves[1])
-                } else if caves[1] == "start" {
-                    dict["start", default: []].append(caves[0])
-                } else if caves[0] == "end" {
-                    dict[caves[1], default: []].append("end")
-                } else if caves[1] == "end" {
-                    dict[caves[0], default: []].append("end")
+            .reduce(into: [Cave: [Cave]]()) { dict, line in
+                let caves = line.components(separatedBy: "-").map(Cave.init)
+                if caves[0].name == "start" {
+                    dict[caves[0], default: []].append(caves[1])
+                } else if caves[1].name == "start" {
+                    dict[caves[1], default: []].append(caves[0])
+                } else if caves[0].name == "end" {
+                    dict[caves[1], default: []].append(caves[0])
+                } else if caves[1].name == "end" {
+                    dict[caves[0], default: []].append(caves[1])
                 } else {
                     dict[caves[0], default: []].append(caves[1])
                     dict[caves[1], default: []].append(caves[0])
@@ -25,61 +76,52 @@ public class Puzzle {
     }
 
     var paths = 0
-    func traverse(current: String, visited: [String]) {
-        var _visited = visited
-        _visited.append(current)
-
-        if current == "end" {
+    func traverse(current: CaveNode) {
+        if current.isEnd {
             paths += 1
         } else {
-            let neighbors = map[current]!.filter { cave in
-                String.isUppercase(string: cave)
-                    || !_visited.contains(cave)
-            }
-            for n in neighbors {
-                traverse(current: n, visited: _visited)
-            }
+            map[current.cave]!
+                .lazy
+                .filter { cave in
+                    cave.isBig
+                        || !current.pathContains(cave: cave)
+                }
+                .forEach { cave in
+                    let node = CaveNode(cave)
+                    current.addChild(caveNode: node)
+                    traverse(current: node)
+                }
         }
     }
 
     public func part1() -> Int {
-        traverse(current: "start", visited: [])
+        traverse(current: CaveNode(Cave("start")))
         return paths
     }
 
-    func hasDoubleVistedLowercaseCaves(path: [String]) -> Bool {
-        var v = Set<String>()
-        for cave in path.filter({ String.isLowercase(string: $0) }) {
-            if !v.insert(cave).inserted {
-                return true
-            }
-        }
-        return false
-    }
-
     var paths2 = 0
-    func traverse2(current: String, visited: [String], hasDouble: Bool) {
-        var _visited = visited
-        _visited.append(current)
-        let canAddDouble = !hasDouble && !hasDoubleVistedLowercaseCaves(path: _visited)
-        if current == "end" {
+    func traverse2(current: CaveNode, hasDouble: Bool) {
+        let canAddDouble = !hasDouble && !current.pathContainsDoubleVisitedSmallCaves()
+        if current.isEnd {
             paths2 += 1
         } else {
-            map[current]!
+            map[current.cave]!
                 .lazy
                 .filter { cave in
-                    String.isUppercase(string: cave)
-                        || !_visited.contains(cave)
+                    cave.isBig
+                        || !current.pathContains(cave: cave)
                         || canAddDouble
                 }
-                .forEach { n in
-                    traverse2(current: n, visited: _visited, hasDouble: !canAddDouble)
+                .forEach { cave in
+                    let node = CaveNode(cave)
+                    current.addChild(caveNode: node)
+                    traverse2(current: node, hasDouble: !canAddDouble)
                 }
         }
     }
 
     public func part2() -> Int {
-        traverse2(current: "start", visited: [], hasDouble: false)
+        traverse2(current: CaveNode(Cave("start")), hasDouble: false)
         return paths2
     }
 }
